ae6c901178c6039eefee1dfba394d66da17159ce
José Augusto <josemoura212@gmail.com>
2024-03-25 13:47:00 -0300

    Finished

--- node/index.js
+++ node/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mysql = require("mysql");
+
 const app = express();
+
 const port = 3000;
+
 const config = {
   host: "db",
   user: "root",
@@ -8,61 +12,58 @@ const config = {
   database: "nodedb",
 };
 
-const mysql = require("mysql");
-const connection = mysql.createConnection(config);
-
-const create_table = `create table if not exists people(id int not null auto_increment primary key, name varchar(255))`;
-connection.query(create_table);
-
-function insertName() {
-  var names = [
-    "Jose",
-    "Wesley",
-    "Geovanna",
-    "Carlos",
-    "Edson",
-    "Thais",
-    "Safira",
-    "Oliver",
-    "Hanna",
-    "Mariza",
-    "Eleida",
-  ];
-
-  var name = names[Math.floor(names.length * Math.random())];
-  return new Promise((resolve, reject) => {
-    connection.query(`insert into people(name) values('${name}')`, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-function getNames() {
-  return new Promise((resolve, reject) => {
-    connection.query("select name from people", (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    });
-  });
-}
-
 app.get("/", (req, res) => {
-  insertName();
-  getNames().then((names) => {
-    const namesList = names.map((name) => `<li>${name.name}</li>`).join("");
-    res.send(`<h1>Full Cycle</hjson><ul>${namesList}</ul>`);
-  });
+  insertAndListNames(res);
 });
 
 app.listen(port, () => {
   console.log("Rodando na porta " + port);
 });
 
-connection.end();
+var names = [
+  "Jose",
+  "Wesley",
+  "Geovanna",
+  "Carlos",
+  "Edson",
+  "Thais",
+  "Safira",
+  "Oliver",
+  "Hanna",
+  "Mariza",
+  "Eleida",
+];
+
+async function insertAndListNames(res) {
+  const connection = mysql.createConnection(config);
+  const create_table = `create table if not exists people(id int not null auto_increment primary key, name varchar(255))`;
+  connection.query(create_table);
+
+  var name = names[Math.floor(names.length * Math.random())];
+  const insertQuery = `insert into people(name) values('${name}')`;
+
+  connection.query(insertQuery, (error, _results, _fields) => {
+    if (error) throw error;
+
+    console.log(`Inserido ${name}`);
+
+    listNames(res, connection);
+  });
+}
+
+async function listNames(res, connection) {
+  const query = `select name from people`;
+
+  connection.query(query, (error, results, _fields) => {
+    if (error) throw error;
+
+    const names = results.map((result) => result.name);
+
+    res.send(
+      `<h1>Full Cycle Rocks!</h1><ul>${names
+        .map((name) => `<li>${name}</li>`)
+        .join("")}</ul>`
+    );
+    connection.end();
+  });
+}
